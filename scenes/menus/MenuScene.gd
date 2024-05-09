d61324943dfc9c2c2987a18d1a8f173893fef680
nyadderall <2232134@gmail.com>
2024-05-09 12:17:49 -0600

    banger fucking midis (temporary)

--- scenes/menus/MenuScene.gd
+++ scenes/menus/MenuScene.gd
@@ -3,6 +3,9 @@ extends Control
 const PlayScene = "res://scenes/GameInitialize.tscn"
 const SettingsScene = "res://scenes/menus/SettingsScene.tscn"
 
+func _ready():
+	$MidiPlayer.play()
+
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file(PlayScene)
 
